f37b4ab1288a9d24d670512568b9643f649df3f4
Lucas Angelon <lucas.a.arouca@gmail.com>
2015-05-14 14:49:01 +0000

    Working on Homepage
    Added some code in quite a few pages and also started the Seeds file for default values.

--- app/controllers/application_pages_controller.rb
+++ app/controllers/application_pages_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationPagesController < ApplicationController
-  
-  # Skipping the log in requirement 
-  #skip_before_filter :redirect_home
     
   def home
+    @courses = []
+    CourseUser.where(user_id: session[:user_id]).find_each do |c|
+      @courses.push(c.course_id)
+    end
+    
+    @courseTitles = []
+    @courses.each do |c|
+      @courseTitles.push(Course.find(c))
+    end
+    
+    
+    #@courseTitles = Course.where(id: courses.course_id)
+    @clusters = CourseCluster.find_by([:course_id][1])
+    @clusterUnits = ClusterUnit.find_by([:cluster_id][1])
+    @units = Unit.last(3)
   end
 
   def quizzes
